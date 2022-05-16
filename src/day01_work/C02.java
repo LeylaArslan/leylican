e3851815843f3269e456dbb833c19ae210755342
FikretArslan <arslanfikret33@gmail.com>
2022-05-16 22:11:15 +0300

    fikret

--- src/day01_work/C02.java
+++ src/day01_work/C02.java
@@ -2,6 +2,11 @@ package day01_work;
 
 public class C02 {
     public static void main(String[] args) {
-        System.out.println("leyla");
+        System.out.println("leyla2");
+        System.out.println("leyla2");
+        System.out.println("leyla2");
+        System.out.println("leyla2");
+        System.out.println("leyla2");
+        System.out.println("leyla2");
     }
 }
