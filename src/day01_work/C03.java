bec33fcc5cf8d434de4771ab59de9a34b058146a
FikretArslan <arslanfikret33@gmail.com>
2022-05-16 22:43:25 +0300

    fikret

---
+++ src/day01_work/C03.java
@@ -0,0 +1,23 @@
+package day01_work;
+
+public class C03 {
+    public static void main(String[] args) {
+
+        System.out.println("fikret");
+        System.out.println("fikret");
+        System.out.println("fikret");
+        System.out.println("fikret");
+        System.out.println("fikret");
+        System.out.println("fikret");
+        System.out.println("fikret");
+        System.out.println("fikret");
+        System.out.println("fikret");
+        System.out.println("fikret");
+        System.out.println("fikret");
+        System.out.println("fikret");
+        System.out.println("fikret");
+        System.out.println("fikret");
+        System.out.println("fikret");
+        System.out.println("fikret");
+    }
+}
